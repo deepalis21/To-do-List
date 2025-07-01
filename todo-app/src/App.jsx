08c70fecd71faf53d23990333c67b9e44a704102
Deepali Singh <lovely@Deepalis-MacBook-Air.local>
2025-07-01 21:11:00 +0530

    Rough Sketch

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      gfn
+    </div>
+  )
+}
+
+export default App
---
+++ todo-app/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // adjust if you use .jsx/.tsx paths
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
